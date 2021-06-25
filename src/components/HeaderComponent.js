7624a68c77519ef2c5b4b7c96cd8b8872ce7320f
Christopher Nolan X <chris.hyatt7@gmail.com>
2021-06-25 17:18:11 -0400

    Adding to the Single Page App

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -55,7 +55,7 @@ class Header extends Component {
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/contacts">
+                  <NavLink className="nav-link" to="/contactus">
                     <i className="fa fa-address-card fa-lg" /> Contact Us
                   </NavLink>
                 </NavItem>
