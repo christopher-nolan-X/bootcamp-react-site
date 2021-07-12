eaa2d88689ec566175c6f417d22c8dfed4a59dfb
Christopher Nolan X <chris.hyatt7@gmail.com>
2021-07-11 21:16:25 -0400

    Fetch Post Comment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,7 +7,7 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { addComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+  postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
+  // addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
   fetchCampsites: () => (fetchCampsites()),
   resetFeedbackForm: () => (actions.reset('feedbackForm')),
   fetchComments: () => (fetchComments()),
@@ -60,7 +61,7 @@ class Main extends Component {
             errMess={this.props.campsites.errMess}
             comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
             commentsErrMess={this.props.comments.errMess}
-            addComment={this.props.addComment}
+            postComment={this.props.postComment}
         />
       );
     }
